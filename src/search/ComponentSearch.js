225ee23b186398fbc34108720634e26f1599e37c
Tim Isakzhanov <timur.isakzhanov@zensoft.io>
2017-02-24 16:20:13 +0600

    style search screen

--- src/search/ComponentSearch.js
+++ src/search/ComponentSearch.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react'
 import {
   StyleSheet,
+  Dimensions,
   View,
   Text,
+  Image,
   TouchableHighlight
 } from 'react-native'
 
@@ -16,13 +18,17 @@ export default class ComponentSearch extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ComponentSearchHeader text='Look for your favorites artists'/>
+        <View style={styles.bg_container}>
+          <Image source={require('../res/img/background_search.png')} style={styles.background} />
+        </View>
+
+        <Text onPress={ this.props.onLogoutClick } style={styles.logout}>Log out</Text>
+
+        <View style={{flex: 1, alignItems: 'center'}}>
+        <Image source={require('../res/img/logo.png')} style={styles.logo} />
+        </View>
 
         <ContainerSearchQueryInput placeholder='Artist name' onRouteUpdated={()=>this.props.onSearchClick()} />
-
-        <TouchableHighlight onPress={ this.props.onLogoutClick }>
-          <Text style={{ fontSize: 20, marginTop: 50 }}>Log out</Text>
-        </TouchableHighlight>
       </View>
     )
   }
@@ -33,12 +39,32 @@ ComponentSearch.propTypes = {
   onLogoutClick: PropTypes.func.isRequired
 }
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#dedede',
     flex: 1,
-    paddingLeft: 8,
-    paddingRight: 8
+  },
+  background: {
+    position: 'absolute',
+    top: 0, bottom: 0, left: 0, right: 0,
+    width: windowWidth,
+    height: windowHeight,
+    resizeMode: "stretch"
+  },
+  logo: {
+    height: 70,
+    width: 70,
+    marginTop: 30,
+    resizeMode: 'center'
+  },
+  logout: {
+    color: '#ffffff',
+    fontSize: 16,
+    textAlign: 'right',
+    marginTop: 12,
+    marginRight: 24
   }
+
 });
--- src/search/ComponentSearchQueryInput.js
+++ src/search/ComponentSearchQueryInput.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import {
+  StyleSheet,
   View,
   Text,
   TextInput,
@@ -14,27 +15,76 @@ export default class ComponentSearchQueryInput extends Component {
 
   render() {
     return (
-      <View>
-        <TextInput style={{marginTop: 20 }}
+      <View style={styles.search_btn_container}>
+        <Text style={styles.message}>Just hit play to stream anything you like.</Text>
+
+        <TextInput style={styles.input}
           placeholder={this.props.placeholder}
+          placeholderTextColor='#ea2859'
+          underlineColorAndroid='#ea2859'
           onChangeText={(text)=> {
             this.query = text
           }} />
 
-          <TouchableHighlight onPress={()=>{
-            if (this.query != null && this.query !== '') {
-              this.props.onSearchClick(this.query),
-              this.props.onRouteUpdated()
-            } else {
-              //ask user to fill input
-            }
-          }}>
-            <Text style={{ fontSize: 20 }}>Search</Text>
-          </TouchableHighlight>
+          <Text style={ styles.search_btn } onPress={()=>{this.search()}}>Search</Text>
         </View>
     )
   }
+
+  search() {
+    console.log("search")
+    if (this.query != null && this.query !== '') {
+
+      this.props.onSearchClick(this.query),
+      this.props.onRouteUpdated()
+    } else {
+      //ask user to fill input
+    }
+  }
 }
+
+const styles = StyleSheet.create({
+  search_btn_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  search_btn: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 50,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius:50,
+    fontSize: 24,
+    color: '#ffffff',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    backgroundColor: '#ff245a',
+  },
+  input: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 100,
+    fontSize: 20,
+    color: '#ea2859',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+  },
+  message: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 50,
+    fontSize: 18,
+    color: '#ffffff',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+  }
+})
 
 ComponentSearchQueryInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
