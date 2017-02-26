80cb5327082d33f58e6556a9bbb4b7b6c9b66f73
Tim Isakzhanov <timur.isakzhanov@zensoft.io>
2017-02-26 22:35:20 +0600

    reducersArtist tests

---
+++ __tests__/reducersArtist.js
@@ -0,0 +1,70 @@
+import artist from '../src/artist/ReducersArtist'
+import {
+  selectArtist,
+  startFetchAlbums,
+  fetchAlbumsError,
+  fetchAlbumsSuccess } from '../src/artist/ActionsArtist'
+
+describe('artist reducer', () => {
+
+  let emptyState
+
+  beforeEach(() => {
+    emptyState = {
+        item: {},
+        isAlbumsFetching: false,
+        albumsFetchingError: '',
+        albums: []
+    }
+  })
+
+  it('should return initial state', () => {
+    expect(artist(undefined, {})).toEqual(emptyState)
+  })
+
+  it ('should handle SELECT_ARTIST', () => {
+    let artistObj = { name: 'validArtist' }
+    let selectArtistAction = selectArtist(artistObj)
+
+    expect(artist(undefined, selectArtistAction))
+      .toEqual({
+        item: artistObj,
+        isAlbumsFetching: false,
+        albumsFetchingError: '',
+        albums: []
+      })
+  })
+
+  it ('should handle FETCH_ALBUMS_START', () => {
+    let fetchAlbumsStartAction = startFetchAlbums()
+
+    expect(artist(emptyState, fetchAlbumsStartAction))
+      .toEqual({
+        item: {},
+        isAlbumsFetching: true,
+        albumsFetchingError: '',
+        albums: []
+      })
+  })
+
+  it ('should handle FETCH_ALBUMS_ERROR', () => {
+    expect(artist(undefined, fetchAlbumsError("test error")))
+      .toEqual({
+        item: {},
+        isAlbumsFetching: false,
+        albumsFetchingError: 'test error',
+        albums: []
+      })
+  })
+
+  it ('should handle FETCH_ALBUMS_SUCCESS', () => {
+    let albums = {items: [{album: 'successAlbum'}]}
+    expect(artist(undefined, fetchAlbumsSuccess(albums)))
+      .toEqual({
+        item: {},
+        isAlbumsFetching: false,
+        albumsFetchingError: '',
+        albums: [{album: 'successAlbum'}]
+      })
+  })
+})
